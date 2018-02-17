75664cea8c3091cdca39c87cf36a07900e1f1f4b
SKapal <sahilkapal@cmail.carleton.ca>
2018-02-17 15:39:07 -0500

    Updated UI with a button that will send a test notification

--- app/src/main/java/com/example/rickydam/waitassistant/MainActivity.java
+++ app/src/main/java/com/example/rickydam/waitassistant/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.rickydam.waitassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button notif;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notif = (Button) findViewById(R.id.not_sender);
+
+        notif.setOnClickListener(v -> {
+            System.out.println("NOTIFICATION");
+        });
+
     }
 }
