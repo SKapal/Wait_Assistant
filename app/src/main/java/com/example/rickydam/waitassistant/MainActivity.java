9ace2aaf727353c7dfc13e14e31a2a09425ab701
SKapal <sahilkapal@cmail.carleton.ca>
2018-02-17 16:15:05 -0500

    Added pinpoint push notification from AWS and firebase

--- app/src/main/java/com/example/rickydam/waitassistant/MainActivity.java
+++ app/src/main/java/com/example/rickydam/waitassistant/MainActivity.java
@@ -3,15 +3,18 @@ package com.example.rickydam.waitassistant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import com.amazonaws.mobile.client.AWSMobileClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button notif;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AWSMobileClient.getInstance().initialize(this).execute();
 
         notif = (Button) findViewById(R.id.not_sender);
 
