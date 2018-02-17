aa696e211a931b9841bf32156b7ba4e9eab098ea
SKapal <sahilkapal@cmail.carleton.ca>
2018-02-17 16:38:36 -0500

    Added pinpoint notification service and made PushListener.java class

--- app/src/main/java/com/example/rickydam/waitassistant/MainActivity.java
+++ app/src/main/java/com/example/rickydam/waitassistant/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.rickydam.waitassistant;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobileconnectors.pinpoint.PinpointConfiguration;
+import com.amazonaws.mobileconnectors.pinpoint.PinpointManager;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+import com.google.android.gms.iid.InstanceID;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button notif;
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    public static PinpointManager pinpointManager;
 
 
     @Override
@@ -17,6 +26,32 @@ public class MainActivity extends AppCompatActivity {
         AWSMobileClient.getInstance().initialize(this).execute();
 
         notif = (Button) findViewById(R.id.not_sender);
+
+        if (pinpointManager == null) {
+            PinpointConfiguration pinpointConfig = new PinpointConfiguration(
+                    getApplicationContext(),
+                    AWSMobileClient.getInstance().getCredentialsProvider(),
+                    AWSMobileClient.getInstance().getConfiguration());
+
+            pinpointManager = new PinpointManager(pinpointConfig);
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        String deviceToken =
+                                InstanceID.getInstance(MainActivity.this).getToken(
+                                        "123456789Your_GCM_Sender_Id",
+                                        GoogleCloudMessaging.INSTANCE_ID_SCOPE);
+                        Log.e("NotError", deviceToken);
+                        pinpointManager.getNotificationClient()
+                                .registerGCMDeviceToken(deviceToken);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
 
         notif.setOnClickListener(v -> {
             System.out.println("NOTIFICATION");
